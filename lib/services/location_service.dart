c2407a1b60c49299033c70f5eccde1f29a5978a7
Stefan Brinkmann <steffomix@gmail.com>
2026-06-19 08:37:32 +0200

    added gps position cache

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,8 +1,18 @@
 import 'package:geolocator/geolocator.dart';
 
+import 'settings_service.dart';
+
+class RecentGPSPoint {
+  final Position position;
+  final DateTime timestamp;
+
+  RecentGPSPoint(this.position, this.timestamp);
+}
+
 class LocationService {
   LocationService._();
   static final LocationService instance = LocationService._();
+  static RecentGPSPoint? lastPoint;
 
   static const LocationSettings _settings = LocationSettings(
     accuracy: LocationAccuracy.high,
@@ -13,12 +23,20 @@ class LocationService {
       Geolocator.getPositionStream(locationSettings: _settings);
 
   Future<Position?> getCurrentPosition() async {
+    final interval = SettingsService.instance.gpsIntervalSeconds * 1000;
+    if (lastPoint != null &&
+        DateTime.now().difference(lastPoint!.timestamp).inMilliseconds <
+            interval * 0.8) {
+      return lastPoint!.position;
+    }
     try {
-      return await Geolocator.getCurrentPosition(
+      final position = await Geolocator.getCurrentPosition(
         locationSettings: const LocationSettings(
           accuracy: LocationAccuracy.high,
         ),
       );
+      lastPoint = RecentGPSPoint(position, DateTime.now());
+      return position;
     } catch (_) {
       return null;
     }
